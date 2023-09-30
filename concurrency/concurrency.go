6ad0af69a58327ee1762bab7ef9c070abfb2d4e0
Andy <andynguyen@axon.com>
2023-09-30 21:30:05 +0700

    concurrency add Runnable

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,5 +1,23 @@
 package concurrency
 
+import "sync"
+
 func PackageInfo() string {
 	return "concurrency has NOT been implemented!"
+}
+
+type Runnable struct {
+	wg sync.WaitGroup
+}
+
+func (r *Runnable) Run(f func()) {
+	r.wg.Add(1)
+	go func() {
+		defer r.wg.Done()
+		f()
+	}()
+}
+
+func (r *Runnable) Wait() {
+	r.wg.Wait()
 }
